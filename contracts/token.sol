70941efeb464d53af7bc1f429508f92696641c35
Liyb2002 <yuanboli@126.com>
2021-08-15 00:10:45 +0800

    update token.sol

--- contracts/token.sol
+++ contracts/token.sol
@@ -9,9 +9,7 @@ contract techX is ERC20 {
     string public constant symbol = "OGift";
     uint public constant supply = 1 * 10**8 * 10**uint(decimals); // 100 million
 
-    /**
-     * @dev ERC20 token to mock techX, send all balance to the deployer
-     */
+
     constructor() public {
         _balances[msg.sender] = supply;
         _totalSupply = supply;
