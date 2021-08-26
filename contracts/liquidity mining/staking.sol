05a463f527d3d79463cbd03fa100285184737589
Liyb2002 <yuanboli@126.com>
2021-08-26 19:03:23 +0800

    +view functions

--- contracts/liquidity mining/staking.sol
+++ contracts/liquidity mining/staking.sol
@@ -11,7 +11,6 @@ contract statking{
       using SafeMath for uint256;
 
     TokenPool public stakingPool;
-    TokenPool public lockedPool;
     TokenPool public unlockedPool;
     mapping (address => UserDetail) UserInfo;
     uint256 private _totalTimeAmount=0;
@@ -34,21 +33,40 @@ contract statking{
     constructor (IERC20 stakingToken, IERC20 MinningToken, 
         uint256 distributionAmount) public{
         stakingPool = new TokenPool(stakingToken);
-        lockedPool=new TokenPool(MinningToken);
         unlockedPool=new TokenPool(MinningToken);
         _Initime=now;
         _distributionAmount=distributionAmount;
 
     }
     
+    //View functions
     function totalStaked() public view returns(uint256){
-        return totalStakedAmount;
+        return (totalStakedAmount);
     }
 
+    function myStakings() public view returns(uint256){
+        return (_viewUserStaking(msg.sender));
+    }
+
+    function myRewards() public view returns(uint256){
+        return (_rewardsCalculation(msg.sender));
+    }
+
+    //External functions called by user
     function stake(uint256 amount)external{
         _stake(msg.sender, amount);
     }
+
+    function claim()external{
+        _claim(msg.sender);
+    }
+
+    function claimAndUnstake()external{
+        _claim(msg.sender);
+        _unstake(msg.sender);
+    }
     
+    //Private functions
     function _stake(address from, uint256 amount)private{
         require(stakingPool.token().transferFrom(from, address(stakingPool),amount), 
         "You are not staking the right amount");
@@ -93,5 +111,29 @@ contract statking{
         return (UserInfo[user].UsertimeAmount).div(totalStakedAmount).mul(_totalAvalAmount);
 
     }
+
+    function _claim(address user)private {
+        _updateUserReward(user);
+        uint256 reward = _rewardsCalculation(user);
+        UserInfo[user].UsertimeAmount=0;
+        UserInfo[user].LastTime=now;
+        unlockedPool.transfer(user, reward);
+    }
+
+    function _unstake(address user) private {
+        uint256 temptAmount = UserInfo[user].stakingAmount;
+        UserInfo[user].stakingAmount=0;
+        totalStakedAmount = totalStakedAmount.sub(temptAmount);
+        stakingPool.transfer(user, temptAmount);
+
+    }
+
+    function _viewUserStaking(address user)view 
+    private returns(uint256){
+        return (UserInfo[user].stakingAmount);
+    }
+
+
+
 
 }
