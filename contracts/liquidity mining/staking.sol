26d97632b5eea972c171486e901e222e26ec6620
Liyb2002 <yuanboli@126.com>
2021-08-26 17:20:29 +0800

    +totalStakedAmount

--- contracts/liquidity mining/staking.sol
+++ contracts/liquidity mining/staking.sol
@@ -14,12 +14,14 @@ contract statking{
     TokenPool public lockedPool;
     TokenPool public unlockedPool;
     mapping (address => UserDetail) UserInfo;
+    uint256 private totalTimeAmount;
+    uint256 private lastTimeStamp;
 
 
     struct UserDetail{
         uint256 stakingAmount;
         uint256 LastTime;
-        uint256 rewards;
+        uint256 UsertimeAmount;
     }
     
     uint256 public totalStakedAmount;
@@ -28,17 +30,25 @@ contract statking{
         stakingPool = new TokenPool(stakingToken);
         lockedPool=new TokenPool(MinningToken);
         unlockedPool=new TokenPool(MinningToken);
+        totalStakedAmount=0;
+        totalTimeAmount=0;
     }
     
     function totalStaked() public view returns(uint256){
         return totalStakedAmount;
     }
+
+    function stake(uint256 amount)external{
+        _stake(msg.sender, amount);
+    }
     
     function _stake(address from, uint256 amount)private{
         require(stakingPool.token().transferFrom(from, address(stakingPool),amount), 
         "You are not staking the right amount");
+        totalStakedAmount = totalStakedAmount.add(amount);
         _updateUserReward(from);
         _updateUserStaking(amount, from);
+        _updateTotalTimeAmount(amount, true);
 
     }
     
@@ -50,9 +60,24 @@ contract statking{
 
     function _updateUserReward(address user) private{
         //Update reward
-        UserInfo[user].rewards=(now-UserInfo[user].LastTime).mul(UserInfo[user].stakingAmount);
+        UserInfo[user].UsertimeAmount=UserInfo[user].UsertimeAmount.add(
+            (now-UserInfo[user].LastTime).mul(UserInfo[user].stakingAmount));
+            
         //Update time
         UserInfo[user].LastTime=now;
+
+    }
+
+    function _updateTotalTimeAmount(uint256 amount, bool isAdd)private{
+        totalTimeAmount= totalTimeAmount.add((now.sub(lastTimeStamp)).mul(totalStakedAmount));
+        lastTimeStamp=now;
+        if(isAdd){
+         totalStakedAmount = totalStakedAmount.add(amount);
+        }
+        else{
+         totalStakedAmount = totalStakedAmount.sub(amount);
+
+        }
 
     }
 
